2a999d251ee36fe2a25cf365db6abf017b5961c7
danxshap <registration@dshap.com>
2015-04-06 10:55:59 -0400

    updates

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-var app = angular.module('main', ['ngTable']).
-controller('testcontrol',function($scope){
+var app = angular.module('main', ['ngTable']);
 
+app.controller('MainCtrl', function($scope, $filter, $http, ngTableParams) {
 
-console.log('sss');
-console.log($scope.page==$scope.params.page());
-console.log($scope.params.page());
+// Keep track of the last page number so we can conditionally show a div
+$scope.lastPage = null;
 
+// Called for every page, via ng-init in the pagination template, so that we can figure out the last page and keep track of it in $scope.lastPage (above)
+$scope.saveLastPage = function(page){
+    if(page.type == 'last'){
+        $scope.lastPage = page.number;
+    }
+};
 
-
-
-
-    }).controller('DemoCtrl', function($scope, $filter, $http,ngTableParams) {
 var mydata = [];
 var sortsetting={Corresp: 'asc'};
 var thefiltersetting={Corresp: ' '};
- 
+
    $scope.save = function(){
-
-
      console.log($scope.mytableParams.data);
- 
-     $http.post('savedata.php',mydata);
-     
 
+     $http.post('savedata.php',mydata);
    }
 
-   
     $http.get("current.json",{header : {'Content-Type' : 'application/json; Charset=UTF-8'}}).success(function(response){
             console.log(response);
              mydata = response;
@@ -41,21 +37,21 @@ var thefiltersetting={Corresp: ' '};
         count: 10,          // count per page
         filter:   thefiltersetting,       // initial filter
         sorting:  sortsetting     // initial sorting
-        
+
     }, {
         total: 0, // length of data
         getData: function($defer, params) {
-      
+
         console.log ("params.sorting",params.sorting());
          console.log ("sort setting",sortsetting);
-         
+
          if (JSON.stringify(sortsetting) != JSON.stringify(params.sorting())){
 
          console.log ('changed!');
          sortsetting=params.sorting();
          params.page(1);
-         
-         
+
+
          }
 
 
@@ -67,15 +63,15 @@ var thefiltersetting={Corresp: ' '};
          console.log ('changed!');
          thefiltersetting=params.filter();
          params.page(1);
-         
-         
+
+
          }
 
 
-         
-        
-          
-       
+
+
+
+
 
             // use build-in angular filter
             var filteredData = params.filter() ?
@@ -87,13 +83,13 @@ var thefiltersetting={Corresp: ' '};
 
             params.total(orderedData.length); // set total for recalc pagination
             $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-     
 
 
 
 
 
-        
+
+
 
 
       }
